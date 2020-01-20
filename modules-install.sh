d396838d449be45167354fda4fb7a95c4542073f
nexuspr <45781670+nexuspr@users.noreply.github.com>
2020-01-19 22:44:06 -0800

    Add files via upload

---
+++ modules-install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#packages
+
+echo "Hi, $USER !"
+echo "Installing Python Modules..."
+echo "Installing datetime"
+python -m pip install datetime
+echo "Installing requests"
+python -m pip install requests
+echo "Installing urllib3"
+python -m pip install urllib3
+echo "Installing xlrd"
+python -m pip install xlrd
+echo "Installing pandas"
+python -m pip install pandas
+echo "Installing xlwt"
+python -m pip install xlwt
+echo "**********************"
+echo "Auto-Install Completed"
+echo "**********************"
